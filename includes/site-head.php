7a8fdf7542170440ea769532e42b610e82501c0b
ParanoirDesign <43961427+ParanoirDesign@users.noreply.github.com>
2026-06-24 15:36:18 +0200

    Charger les composants CSS partagés

--- includes/site-head.php
+++ includes/site-head.php
@@ -13,5 +13,6 @@ $pageDescription = $pageDescription ?? 'Paranoir Studio clarifie votre business 
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Sora:wght@500;600;700;800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/css/site.css">
+<link rel="stylesheet" href="/assets/css/components.css">
 </head>
 <body class="site-page">
